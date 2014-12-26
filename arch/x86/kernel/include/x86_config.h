7dbbe47892a012b10682ddc9fc44d102ed8254e0
Min Le <lemin9538@gmail.com>
2014-12-26 21:31:15 +0800

    add some code for x86 at 2014/12/26
    Signed-off-by: Min Le <lemin9538@gmail.com>

---
+++ arch/x86/kernel/include/x86_config.h
@@ -0,0 +1,20 @@
+/*
+ * arch/x86/kernel/x86_config.h
+ *
+ * Created by Le Min at 2014/12/26
+ */
+
+#ifndef __X86_CONFIG_H_
+#define __X86_CONFIG_H_
+
+#define KERNEL_PDE_BASE			0xa0000000
+#define KERNEL_PDE_SIZE			0x1000
+
+#define KERNEL_STACK_SIZE		0x2000
+#define KERNEL_STACK_BASE		(KERNEL_PDE_BASE + KERNEL_PDE_SIZE + KERNEL_STACK_SIZE)
+
+#define SYSTEM_GDT_BASE			(KERNEL_STACK_BASE + KERNEL_STACK_SIZE)
+#define SYSTEM_GDT_SIZE			0x1000
+
+#define SYSTEM_IDT_BASE			(KERNEL_GDT_BASE + SYSTEM_GDT_SIZE)
+#define SYSTEM_IDT_SIZE			0x1000
